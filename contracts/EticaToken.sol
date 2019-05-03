35f403fb5d63ca1ce02581e2709f4fca9d1c5ef9
kevin-wad <kevin.osseni@groupe-quintesens.fr>
2019-05-03 18:26:05 +0200

    Implementation of 0xbitcoin mining features in process. State variables and mining functions added to EticaToken smart contract

--- contracts/EticaToken.sol
+++ contracts/EticaToken.sol
@@ -102,9 +102,58 @@ contract EticaToken is ERC20Interface{
 
     //allowed[0x1111....][0x22222...] = 100;
 
+    // 0xbitcoin state variables
+    uint public _totalSupply;
+
+
+
+     uint public latestDifficultyPeriodStarted;
+
+
+
+    uint public epochCount;//number of 'blocks' mined
+
+
+    uint public _BLOCKS_PER_READJUSTMENT = 1024;
+
+
+    //a little number
+    uint public  _MINIMUM_TARGET = 2**16;
+
+
+      //a big number is easier ; just find a solution that is smaller
+    //uint public  _MAXIMUM_TARGET = 2**224;  bitcoin uses 224
+    uint public  _MAXIMUM_TARGET = 2**234;
+
+
+    uint public miningTarget;
+
+    bytes32 public challengeNumber;   //generate a new one when a new reward is minted
+
+
+
+    uint public rewardEra;
+    uint public maxSupplyForEra;
+
+
+    address public lastRewardTo;
+    uint public lastRewardAmount;
+    uint public lastRewardEthBlockNumber;
+
+    bool locked = false;
+
+    mapping(bytes32 => bytes32) solutionForChallenge;
+
+    uint public tokensMinted;
+
+    // 0xbitcoin state variables
+
+
+
 
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    event Mint(address indexed from, uint reward_amount, uint epochCount, bytes32 newChallengeNumber);
 
 
     constructor() public{
@@ -129,6 +178,27 @@ contract EticaToken is ERC20Interface{
 
 
       // phase1 mining
+
+      _totalSupply = 21000000 * 10**uint(decimals);
+
+      if(locked) revert();
+      locked = true;
+
+      tokensMinted = 0;
+
+      rewardEra = 0;
+      maxSupplyForEra = _totalSupply.div(2);
+
+      miningTarget = _MAXIMUM_TARGET;
+
+      latestDifficultyPeriodStarted = block.number;
+
+      _startNewMiningEpoch();
+
+
+      //The owner gets nothing! You must mine this ERC20 token
+      //balances[owner] = _totalSupply;
+      //Transfer(address(0), owner, _totalSupply);
 
 
       // PHASE 1
@@ -195,4 +265,245 @@ contract EticaToken is ERC20Interface{
 
          return true;
      }
+
+
+     // -------------  0xbitcoin functions ---------------- //
+
+         function mint(uint256 nonce, bytes32 challenge_digest) public returns (bool success) {
+
+
+             //the PoW must contain work that includes a recent ethereum block hash (challenge number) and the msg.sender's address to prevent MITM attacks
+             bytes32 digest =  keccak256(challengeNumber, msg.sender, nonce );
+
+             //the challenge digest must match the expected
+             if (digest != challenge_digest) revert();
+
+             //the digest must be smaller than the target
+             if(uint256(digest) > miningTarget) revert();
+
+
+             //only allow one reward for each challenge
+              bytes32 solution = solutionForChallenge[challengeNumber];
+              solutionForChallenge[challengeNumber] = digest;
+              if(solution != 0x0) revert();  //prevent the same answer from awarding twice
+
+
+             uint reward_amount = getMiningReward();
+
+             balances[msg.sender] = balances[msg.sender].add(reward_amount);
+
+             tokensMinted = tokensMinted.add(reward_amount);
+
+
+             //Cannot mint more tokens than there are
+             assert(tokensMinted <= maxSupplyForEra);
+
+             //set readonly diagnostics data
+             lastRewardTo = msg.sender;
+             lastRewardAmount = reward_amount;
+             lastRewardEthBlockNumber = block.number;
+
+
+              _startNewMiningEpoch();
+
+               Mint(msg.sender, reward_amount, epochCount, challengeNumber );
+
+            return true;
+
+         }
+
+
+     //a new 'block' to be mined
+     function _startNewMiningEpoch() internal {
+
+       //if max supply for the era will be exceeded next reward round then enter the new era before that happens
+
+       //40 is the final reward era, almost all tokens minted
+       //once the final era is reached, more tokens will not be given out because the assert function
+       if( tokensMinted.add(getMiningReward()) > maxSupplyForEra && rewardEra < 39)
+       {
+         rewardEra = rewardEra + 1;
+       }
+
+       //set the next minted supply at which the era will change
+       // total supply is 2100000000000000  because of 8 decimal places
+       maxSupplyForEra = _totalSupply - _totalSupply.div( 2**(rewardEra + 1));
+
+       epochCount = epochCount.add(1);
+
+       //every so often, readjust difficulty. Dont readjust when deploying
+       if(epochCount % _BLOCKS_PER_READJUSTMENT == 0)
+       {
+         _reAdjustDifficulty();
+       }
+
+
+       //make the latest ethereum block hash a part of the next challenge for PoW to prevent pre-mining future blocks
+       //do this last since this is a protection mechanism in the mint() function
+       challengeNumber = block.blockhash(block.number - 1);
+
+
+
+
+
+
+     }
+
+
+
+
+     //https://en.bitcoin.it/wiki/Difficulty#What_is_the_formula_for_difficulty.3F
+     //as of 2017 the bitcoin difficulty was up to 17 zeroes, it was only 8 in the early days
+
+     //readjust the target by 5 percent
+     function _reAdjustDifficulty() internal {
+
+
+         uint ethBlocksSinceLastDifficultyPeriod = block.number - latestDifficultyPeriodStarted;
+         //assume 360 ethereum blocks per hour
+
+         //we want miners to spend 10 minutes to mine each 'block', about 60 ethereum blocks = one 0xbitcoin epoch
+         uint epochsMined = _BLOCKS_PER_READJUSTMENT; //256
+
+         uint targetEthBlocksPerDiffPeriod = epochsMined * 60; //should be 60 times slower than ethereum
+
+         //if there were less eth blocks passed in time than expected
+         if( ethBlocksSinceLastDifficultyPeriod < targetEthBlocksPerDiffPeriod )
+         {
+           uint excess_block_pct = (targetEthBlocksPerDiffPeriod.mul(100)).div( ethBlocksSinceLastDifficultyPeriod );
+
+           uint excess_block_pct_extra = excess_block_pct.sub(100).limitLessThan(1000);
+           // If there were 5% more blocks mined than expected then this is 5.  If there were 100% more blocks mined than expected then this is 100.
+
+           //make it harder
+           miningTarget = miningTarget.sub(miningTarget.div(2000).mul(excess_block_pct_extra));   //by up to 50 %
+         }else{
+           uint shortage_block_pct = (ethBlocksSinceLastDifficultyPeriod.mul(100)).div( targetEthBlocksPerDiffPeriod );
+
+           uint shortage_block_pct_extra = shortage_block_pct.sub(100).limitLessThan(1000); //always between 0 and 1000
+
+           //make it easier
+           miningTarget = miningTarget.add(miningTarget.div(2000).mul(shortage_block_pct_extra));   //by up to 50 %
+         }
+
+
+
+         latestDifficultyPeriodStarted = block.number;
+
+         if(miningTarget < _MINIMUM_TARGET) //very difficult
+         {
+           miningTarget = _MINIMUM_TARGET;
+         }
+
+         if(miningTarget > _MAXIMUM_TARGET) //very easy
+         {
+           miningTarget = _MAXIMUM_TARGET;
+         }
+     }
+
+
+     //this is a recent ethereum block hash, used to prevent pre-mining future blocks
+     function getChallengeNumber() public constant returns (bytes32) {
+         return challengeNumber;
+     }
+
+     //the number of zeroes the digest of the PoW solution requires.  Auto adjusts
+      function getMiningDifficulty() public constant returns (uint) {
+         return _MAXIMUM_TARGET.div(miningTarget);
+     }
+
+     function getMiningTarget() public constant returns (uint) {
+        return miningTarget;
+    }
+
+
+
+     //21m coins total
+     //reward begins at 50 and is cut in half every reward era (as tokens are mined)
+     function getMiningReward() public constant returns (uint) {
+         //once we get half way thru the coins, only get 25 per block
+
+          //every reward era, the reward amount halves.
+
+          return (50 * 10**uint(decimals) ).div( 2**rewardEra ) ;
+
+     }
+
+     //help debug mining software
+     function getMintDigest(uint256 nonce, bytes32 challenge_digest, bytes32 challenge_number) public view returns (bytes32 digesttest) {
+
+         bytes32 digest = keccak256(challenge_number,msg.sender,nonce);
+
+         return digest;
+
+       }
+
+         //help debug mining software
+       function checkMintSolution(uint256 nonce, bytes32 challenge_digest, bytes32 challenge_number, uint testTarget) public view returns (bool success) {
+
+           bytes32 digest = keccak256(challenge_number,msg.sender,nonce);
+
+           if(uint256(digest) > testTarget) revert();
+
+           return (digest == challenge_digest);
+
+         }
+
+
+
+
+     // ------------------------------------------------------------------------
+
+     // Token owner can approve for `spender` to transferFrom(...) `tokens`
+
+     // from the token owner's account. The `spender` contract function
+
+     // `receiveApproval(...)` is then executed
+
+     // ------------------------------------------------------------------------
+
+     function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
+
+         allowed[msg.sender][spender] = tokens;
+
+         Approval(msg.sender, spender, tokens);
+
+         ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
+
+         return true;
+
+     }
+
+
+
+     // ------------------------------------------------------------------------
+
+     // Don't accept ETH
+
+     // ------------------------------------------------------------------------
+
+     function () public payable {
+
+         revert();
+
+     }
+
+
+
+     // ------------------------------------------------------------------------
+
+     // Owner can transfer out any accidentally sent ERC20 tokens
+
+     // ------------------------------------------------------------------------
+
+     function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
+
+         return ERC20Interface(tokenAddress).transfer(owner, tokens);
+
+     }
+
+// ------------------      0xbitcoin functions   -------------------------  //
+
+
+
 }

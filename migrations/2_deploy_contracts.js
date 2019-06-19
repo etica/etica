d26181af676a241c3b9b14b0c5b546c7b2e1b817
kevin-wad <kevin.osseni@groupe-quintesens.fr>
2019-06-19 12:20:55 +0200

    Implementation of EticaReleaseVotingTest in process

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 var EticaTokenUniqueTest = artifacts.require("./EticaTokenUniqueTest.sol");
 var EticaRelease = artifacts.require("./EticaRelease.sol");
+var EticaReleaseVotingTest = artifacts.require("./EticaReleaseVotingTest.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(EticaTokenUniqueTest);
   deployer.deploy(EticaRelease);
+  deployer.deploy(EticaReleaseVotingTest);
 //  deployer.deploy(EticaToken, "0x64f4Ceede08940292b4d36912F69edD65F9AB645");
 };
---
+++ test/EticaReleaseVotingTest.js
@@ -0,0 +1,216 @@
+var EticaReleaseVotingTest = artifacts.require("./EticaReleaseVotingTest.sol");
+
+var solidityHelper =  require('./solidity-helper');
+var miningHelper =  require('./mining-helper-fast');
+var networkInterfaceHelper =  require('./network-interface-helper');
+
+// test suite
+contract('EticaReleaseVotingTest', function(accounts){
+  var EticaReleaseVotingTestInstance;
+  var i;
+
+  // ENTER USER ACCOUNTS KEYS, FOR EXEMPLE YOU CAN TAKE THE ONE FROM GANACHE
+  var test_account= {
+     'address': '0xBa2bD26950957368558dF231f13C3F767b904EC3',
+     'privateKey': 'a04ea152108d978903f48b00feb753c4108ed3d39c4602d3f3e5b158129fba82'
+ }
+
+ var test_account2= {
+    'address': '0xf7EE6506fE651505B68EE5E9aA42f17E63126156',
+    'privateKey': '3f1f4741a79d1ee425819b304bdcddd189186dd183427218299d4e4325bf0411'
+}
+
+var test_account3= {
+  'address': '0x3D39e128cbA9E431cE0eF2E783E81864fc2e0C34',
+  'privateKey': '44d964eeec131409b26385c2eef1d63f7dfd703967b7af98b567b2d46a151579'
+}
+
+var test_account4= {
+  'address': '0xD79e20A9DEfD15E8502697B55074a822Bf29192c',
+  'privateKey': '26add5648161ce7488712f7e79c80f08358eb169867140200d050d2cff70763d'
+}
+
+var test_account5= {
+  'address': '0xefd3FE5f37b38CC07F156f507eE1519b03317A9B',
+  'privateKey': '24dd10d581b3876ec2922f56c3aa2eee6ba865322d87ad30354544237c6d0062'
+}
+
+var test_account6= {
+  'address': '0x6E68F1EA1a10F8a0093DA68495c118940eF1dC8C',
+  'privateKey': '60718bce44b71e60b464e025111726ba0111efff2b42e33660d1f9deeb7d256c'
+}
+
+var test_account7= {
+  'address': '0x9668546C66D99d8a951E4d94fF778521849190e5',
+  'privateKey': 'c6ce9a8d42662ce7d8b726c4616b1c40b2d8c9a06506862a9e14b7ed76be1de2'
+}
+
+var test_account8= {
+  'address': '0xd13cCB6eA16e2cBE56F95745681Cc667828ecd4E',
+  'privateKey': 'e9007cf61c36436cacd8540f903596aadc67e33cd6437f24211694cae24a018d'
+}
+
+ var miner_account= {
+    'address': '0x5FBd856f7f0c79723100FF6e1450cC1464D3fffC',
+    'privateKey': '16b271fdb3eb17a065d4227a3087fa140ba0f88d0d66e7eaa778e3e5c0c6838c'
+}
+
+var PROPOSAL_DEFAULT_VOTE = 10; // 10 ETI default vote for proposal submissions
+
+
+  it("start mining", async function () {
+
+    var tokenContract = await EticaReleaseVotingTest.deployed();
+
+        networkInterfaceHelper.init(web3,tokenContract,miner_account,accounts);
+        miningHelper.init(web3,tokenContract,miner_account,networkInterfaceHelper);
+
+  });
+
+
+  it("can get stakes length", async function () {
+    return EticaReleaseVotingTest.deployed().then(function(instance){
+      EticaReleaseVotingTestInstance = instance;
+      return EticaReleaseVotingTestInstance.stakescount(test_account.address);
+    }).then(async function(response){
+      //console.log('!!!!!!!!!! test_acount NUMBER OF STAKES !!!!!!!!!!!!!');
+    //console.log('test_acount NUMBER OF STAKES IS', response);
+    let stakesl = await EticaReleaseVotingTestInstance.stakescount(test_account.address);
+    //console.log('stakescount RESPONSE IS::::', stakesl);
+    })
+  });
+
+
+  it("can make initial distribution of ETI :", async function () {
+    console.log('------------------------------------- Starting INITIAL ETI DISTRIBUTION ---------------------------');
+    console.log('........................................  CAN MAKE INITIAL ETI DISTRIBUTION ? .......................');
+    function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// wait long enough so that miner_account has mined a block and thus has ETI available, we need a lot of ETI as all tests of this file assume enough ETI and don't deal with mining tests
+//await timeout(150000);
+return EticaReleaseVotingTest.deployed().then(function(instance){
+  EticaReleaseVotingTestInstance = instance;
+  return EticaReleaseVotingTestInstance.balanceOf(miner_account.address);
+}).then(function(receipt){
+console.log('asserting miner_account balance', web3.utils.fromWei(receipt, "ether" ), 'ETI');
+assert(web3.utils.fromWei(receipt, "ether" ) > 70, 'miner_account should have at least 70 mined ETI after 150000 ms! Please relaunch the test, you will be more lucky next time !');
+}).then(async function(){
+  console.log('transfering 10 ETI from miner_account to test_account');
+  let test_accountbalancebefore = await EticaReleaseVotingTestInstance.balanceOf(test_account.address);
+  let miner_accountbalancebefore = await EticaReleaseVotingTestInstance.balanceOf(miner_account.address);
+  console.log('miner_account ETI balance before:', web3.utils.fromWei(miner_accountbalancebefore, "ether" ));
+  console.log('test_account ETI balance before:', web3.utils.fromWei(test_accountbalancebefore, "ether" ));
+  return EticaReleaseVotingTestInstance.transfer(test_account.address,  web3.utils.toBN(10000000000000000000), {from: miner_account.address});
+
+}).then(async function(){
+  // consoling result of ETI transfer from miner_account to test_account
+  let test_accountbalanceafter = await EticaReleaseVotingTestInstance.balanceOf(test_account.address);
+  let miner_accountbalanceafter = await EticaReleaseVotingTestInstance.balanceOf(miner_account.address);
+  console.log('miner_account ETI balance after:', web3.utils.fromWei(miner_accountbalanceafter, "ether" ));
+  console.log('test_account ETI balance after:', web3.utils.fromWei(test_accountbalanceafter, "ether" ));
+}).then(async function(){
+  console.log('transfering 10 ETI from miner_account to test_account2');
+  let miner_accountbalancebefore = await EticaReleaseVotingTestInstance.balanceOf(miner_account.address);
+  let test_account2balancebefore = await EticaReleaseVotingTestInstance.balanceOf(test_account2.address);
+  console.log('miner_account ETI balance before:', web3.utils.fromWei(miner_accountbalancebefore, "ether" ));
+  console.log('test_account2 ETI balance before:', web3.utils.fromWei(test_account2balancebefore, "ether" ));
+  return EticaReleaseVotingTestInstance.transfer(test_account2.address,  web3.utils.toBN(10000000000000000000), {from: miner_account.address});
+
+}).then(async function(){
+  let miner_accountbalanceafter = await EticaReleaseVotingTestInstance.balanceOf(miner_account.address);
+  let test_account2balanceafter = await EticaReleaseVotingTestInstance.balanceOf(test_account2.address);
+  console.log('miner_account ETI balance after:', web3.utils.fromWei(miner_accountbalanceafter, "ether" ));
+  console.log('test_account2 ETI balance after:', web3.utils.fromWei(test_account2balanceafter, "ether" ));
+  console.log('...................................  TESTS CAN START, ACCOUNTS HAVE BEEN GIVEN ETI FOR TESTING  ....................... ');
+  console.log('---------------------------------------- END OF INITIAL ETI DISTRIBUTION with SUCCESS ----------------------------------------------');
+})
+
+  });
+
+
+  async function printBalances(accounts) {
+    // accounts.forEach(function(ac, i) {
+       var balance_val = await (web3.eth.getBalance(accounts[0]));
+       //console.log('acct 0 balance', web3.utils.fromWei(balance_val.toString() , 'ether') )
+    // })
+   }
+
+   async function advanceseconds(duration) {
+
+   let numberlblocks = uint(duration / 15);
+
+   console.log('numberlblocks is', numberlblocks);
+
+     for(var i=0;i<numberlbocks;i+=1){
+
+
+/*  await web3.currentProvider.send({
+     jsonrpc: '2.0',
+     method: 'evm_increaseTime',
+     params: [15], // each blocks ads 30 seconds
+     id: id,
+   }, err1 => {
+     if (err1) return
+   })*/
+
+   await web3.currentProvider.send({
+       jsonrpc: '2.0',
+       method: 'evm_mine',
+       id: id+1,
+     }, (err2, res) => {
+       return
+     })
+
+
+     }
+
+   }
+
+   async function advanceminutes(duration) {
+
+    let id = Date.now();
+     let numberlblocks = duration * 4; // 4 is because 60 / 15 == 4
+
+     console.log('numberlblocks is', numberlblocks);
+
+       for(var i=0;i<numberlblocks;i+=1){
+
+
+    await web3.currentProvider.send({
+       jsonrpc: '2.0',
+       method: 'evm_increaseTime',
+       params: [15], // each blocks ads 30 seconds
+       id: id,
+     }, err1 => {
+       if (err1) return
+     })
+
+     await web3.currentProvider.send({
+         jsonrpc: '2.0',
+         method: 'evm_mine',
+         id: i+1,
+       }, (err2, res) => {
+         return
+       })
+
+
+       }
+
+   }
+
+   async function advanceblocks(numberlbocks) {
+     let numberlblocks = uint(numberlbocks * 60 / 15);
+     console.log('numberlblocks2 is', numberlblocks);
+     for(var i=0;i<numberlblocks;i+=1){
+    await web3.currentProvider.send(
+      {jsonrpc: "2.0", method: "evm_mine", id: i},
+    (err2, res) => {
+
+  return
+});
+     }
+   }
+
+ });

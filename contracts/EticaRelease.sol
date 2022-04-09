72927b3f8aa2054b21d4d98508f35bdd1583f511
Kevin Wad <nomeail@eticaprotocol.org>
2022-04-09 18:32:59 +0200

    Last Commit

--- contracts/EticaRelease.sol
+++ contracts/EticaRelease.sol
@@ -2,12 +2,12 @@ pragma solidity ^0.5.2;
 
 /*
 ETICA: a type1 civilization neutral protocol for medical research
-KEVIN WAD OSSENI
+KEVIN WAD
 */
 
 /*
 MIT License
-Copyright © 26/08/2019, KEVIN WAD OSSENI
+Copyright © 26/08/2019, KEVIN WAD
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 

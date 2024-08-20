5093b9fbd8a364498de4f2a66aa38108eff439fb
Kevin Wad <noemail@eticaprotocol.org>
2024-08-20 08:26:25 +0200

    make SEEDHASH_EPOCH_BLOCKS public

--- contracts/EticaRelease.sol
+++ contracts/EticaRelease.sol
@@ -694,7 +694,7 @@ bool public UPDATEDV3 = false;
 mapping(bytes32 => mapping(address => bytes32)) public randomxSealSolutions; // randomxSealSolutions['challengeNumber']['mineraddress'] = validatedrandomXsolutionDiggest where validatedrandomXsolutionDiggest is a Keccak256 hash of (validatedrandomXsolution, difficulty)
 bytes public randomxBlob;
 bytes public randomxSeedhash;   //generate a new randomxSeedhash every SEEDHASH_EPOCH_BLOCKS, should always be size bytes32 but use bytes in case randomX changes seedhash size in the future
-uint SEEDHASH_EPOCH_BLOCKS = 410; // Adjusts randomxSeedhash about every 210 days (410/144) = 2.8 days
+uint public SEEDHASH_EPOCH_BLOCKS = 410; // Adjusts randomxSeedhash about every 210 days (410/144) = 2.8 days
 
 // WARNING NEW STORAGE VARIABLES V3 //
 
